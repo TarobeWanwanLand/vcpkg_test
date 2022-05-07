5582c3bdb22bed8047920c4f31fc8832fd2fad41
Tomomi_Murakami <tomomi.murakami1225@gmail.com>
2022-05-07 19:09:42 +0900

    基礎完成

---
+++ cmake_install-debug-visual-studio-x86-shared/include/foo/library.h
@@ -0,0 +1,6 @@
+#ifndef VCPKG_TEST_LIBRARY_H
+#define VCPKG_TEST_LIBRARY_H
+
+void hello();
+
+#endif //VCPKG_TEST_LIBRARY_H
---
+++ source/library.cpp
@@ -0,0 +1,7 @@
+#include "foo/library.h"
+
+#include <iostream>
+
+void hello() {
+    std::cout << "Hello, World!" << std::endl;
+}
